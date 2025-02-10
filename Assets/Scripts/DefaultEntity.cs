54d9b055b95a976474f4b61953ffefaf9248acc1
SAE1\1033265 <Emu>
2025-02-10 13:19:06 +1030

    Added default entity script and imported package.

---
+++ Assets/Scripts/DefaultEntity.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultEntity : MonoBehaviour
+{
+    protected int health=100;
+    protected float moveSpeed=1f;
+    protected Vector3 location;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    protected virtual void Move(Vector3 targetLocation, Vector3 currentLocation)
+    {
+        //moves from the currentLocation to the targetLocation gradually.
+
+    }
+
+    protected virtual int Randomizer(int minNumber, int maxNumber)
+    {
+        int randomNumber=Random.Range(minNumber, maxNumber);
+        Debug.Log("Number was randomized between: " + minNumber + " " + maxNumber + ". The random number is: " + randomNumber);
+        return randomNumber;
+    }
+
+    protected virtual void Attack()
+    {
+
+    }
+}
